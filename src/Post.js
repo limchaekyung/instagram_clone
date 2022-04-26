1d514cf5c9c1139574e327eb31910ac8cfabb669
limchaekyung <corud505@naver.com>
2022-04-26 23:56:45 +0900

    comment1

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Post.css'
 import Avatar from "@material-ui/core/Avatar"
+import { db } from "./firebase"
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, username, caption, imageUrl }) {
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+        let unsubscribe;
+        if (postId) {
+            unsubscribe = db
+                .collection("posts")
+                .doc(postId)
+                .collection("comments")
+                .onSnapshot((snapshot) => {
+                    setComment(snapshot.docs.map((doc) => doc.data()))
+                })
+        }
+        return () => {
+            unsubscribe();
+        }
+    }, [postId])
+
+    const postComment = (event) => {
+        event.preventDefault();
+    }
     return (
         <div className='post'>
             <div className="post__header">
@@ -12,11 +34,36 @@ function Post({ username, caption, imageUrl }) {
             </div>
 
             <img className="post__image" src={imageUrl} alt="" />
-            {/* image */}
 
             <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
 
-            {/* username + caption */}
+            {
+                <div className="post__comments">
+                    {comments.map((comment) => (
+                        <p>
+                            <strong>{comment.username}</strong> {comment.text}
+                        </p>
+                    ))}
+                </div>
+            }
+            <form>
+                <input
+                    className="post__input"
+                    type="text"
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                />
+                <button
+                    display={!comment}
+                    className="post__button"
+                    type="submit"
+                    onClick={postComment}
+                >
+                    Post
+                </button>
+            </form>
+
         </div>
     )
 }
